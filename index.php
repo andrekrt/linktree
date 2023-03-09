90a38114256c57212846d6ad513356941fab15c5
andrekrt <andrekrt1922@gmail.com>
2023-03-09 13:52:07 -0300

    linktree pessoal

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Friobom</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+</head>
+<body>
+    <div class="container">
+        <div class="icone-logo">
+            <img src="assets/images/icon-white.png" alt="">
+        </div>
+        <div class="lista">
+            <div class="item">
+                <div class="icone">
+                    <img src="assets/images/icon-site.png" alt="">
+                </div>
+                <div class="titulo">
+                    <a href="https://friobomdistribuidora.com.br/">Institucional</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="icone">
+                    <img src="assets/images/ecommerce.png" alt="">
+                </div>
+                <div class="titulo">
+                    <a href="https://friobombacabal.com.br/">E-commerce Bacabal</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="icone">
+                    <img src="assets/images/ecommerce.png" alt="">
+                </div>
+                <div class="titulo">
+                    <a href="https://friobomimperatriz.com.br/">E-commerce Imperatriz</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="icone">
+                    <img src="assets/images/whats.png" alt="">
+                </div>
+                <div class="titulo">
+                    <a href="https://wa.me/+5599984698071">Unidade Bacabal</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="icone">
+                    <img src="assets/images/whats.png" alt="">
+                </div>
+                <div class="titulo">
+                    <a href="https://wa.me/+5599991520654" >Unidade Imperatriz</a>
+                </div>
+            </div>
+        </div>
+        <div class="rodape">
+            <img src="assets/images/logo.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
